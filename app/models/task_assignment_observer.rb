b391f18d73166660bbbe623bc04a4b5fe82820e9
brvarner <bvarner4592@gmail.com>
2024-07-22 11:42:50 -0500

    added email sending to observer, added text to plaintext text assignment email

--- app/models/task_assignment_observer.rb
+++ app/models/task_assignment_observer.rb
@@ -1,10 +1,8 @@
 class TaskAssignmentObserver < ActiveRecord::Observer
     def after_create(task_assignment)
         # Here, we trigger an action mailer with the user's email address and the task name/description.
-        task_creator = task_assignment.task.creator
-        project = task_assignment.task.project
-        title = task_assignment.task.title
-        desc = task_assignment.task.description
-        recipient = task_assignment.recipient
+        task = task_assignment.task.id
+        recipient = task_assignment.recipient.id
+        TaskAssignmentMailer.with(user_id: recipient, task_id: task).assignment_email.deliver_now
     end
 end
