3f93c11364b2f0f1929f529140a7ca7cfbffcc5c
brvarner <bvarner4592@gmail.com>
2024-07-31 14:43:49 -0500

    added action mailbox config to config/environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailbox.ingress = :sendgrid
+config.action_mailbox.ingress_password = Rails.application.credentials.dig(:action_mailbox, :ingress_password)
