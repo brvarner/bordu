88dcf171b2ff4bde835024e8beb4cdba54414375
brvarner <bvarner4592@gmail.com>
2024-07-31 14:49:48 -0500

    removed config from config/environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,5 @@
 # Load the Rails application.
-require_relative 'application'
+require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.application.configure do
-  config.action_mailbox.ingress = :sendgrid
-  config.action_mailbox.ingress_password = Rails.application.credentials.dig(:action_mailbox, :ingress_password)
-end
