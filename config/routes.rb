eb7901a6e3784121230f498aa1ebabb1cf5aab24
brvarner <bvarner4592@gmail.com>
2024-08-02 19:34:33 -0500

    trying the action mailbox combo routing

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   mount ActionMailbox::Engine => '/rails/action_mailbox'
+  post '/rails/action_mailbox/mailgun/inbound_emails/mime' => 'action_mailbox/ingresses/mailgun/inbound_emails#create'
 end
