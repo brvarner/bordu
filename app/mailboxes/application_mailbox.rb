8d1122762e0ef1f2572d4f8b6ac0433820b09956
brvarner <bvarner4592@gmail.com>
2024-08-02 19:50:53 -0500

    reconfigured AppMailbox routing

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,4 +1,4 @@
 class ApplicationMailbox < ActionMailbox::Base
   # Routing for emails sent to updates.<task_id>.<user_id>@app.bordu.vip
-  routing(/^updates\.\d+\.\d+@parse\.bordu\.vip$/i => :task_updates)
+  routing(/^updates\.\d+\.\d+@parse\.bordu\.vip$/i => :updates)
 end
