eae509a8fbffac32b478a7985b1a0ffc0663f08c
brvarner <bvarner4592@gmail.com>
2024-08-14 22:39:57 -0500

    added errors controller

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,10 @@
+# app/controllers/errors_controller.rb
+class ErrorsController < ApplicationController
+  def not_found
+    render status: :not_found
+  end
+
+  def internal_server_error
+    render status: :internal_server_error
+  end
+end
