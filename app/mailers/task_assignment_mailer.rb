7d5768c0999d036780168832eec3c153fff312d0
brvarner <bvarner4592@gmail.com>
2024-08-26 12:12:36 -0500

    started adding sign up email for people who don't have accounts

--- app/mailers/task_assignment_mailer.rb
+++ app/mailers/task_assignment_mailer.rb
@@ -1,10 +1,16 @@
 class TaskAssignmentMailer < ApplicationMailer
-  default from: "Bordu <info@bordu.vip>"
+  default from: 'Bordu <info@bordu.vip>'
 
   def assignment_email
     @recipient = User.find(params[:user_id])
     @task = Task.find(params[:task_id])
-    mail(to: @recipient.email, subject: "Task ##{@task.id} - #{@task.title} Assigned To You", reply_to: "'Bordu' <updates.#{@recipient.id}.#{@task.id}@parse.bordu.vip>")
+    mail(to: @recipient.email, subject: "Task ##{@task.id} - #{@task.title} Assigned To You",
+         reply_to: "'Bordu' <updates.#{@recipient.id}.#{@task.id}@parse.bordu.vip>")
   end
 
+  def signup_email
+    @recipient = params[:email]
+    @task = Task.find(params[:task_id])
+    mail(to: @recipient, subject: "#{@task.creator} has invited you to Bordu!")
+  end
 end
--- app/models/task_assignment_observer.rb
+++ app/models/task_assignment_observer.rb
@@ -1,7 +1,12 @@
 class TaskAssignmentObserver < ActiveRecord::Observer
-    def after_create(task_assignment)
-        task = task_assignment.task.id
-        recipient = task_assignment.recipient.id
-        TaskAssignmentMailer.with(user_id: recipient, task_id: task).assignment_email.deliver_later
+  def after_create(task_assignment)
+    task = task_assignment.task.id
+    recipient = task_assignment.recipient.id
+    email = task_assignment.email
+    if recipient.exists?
+      TaskAssignmentMailer.with(user_id: recipient, task_id: task).assignment_email.deliver_later
+    else
+      TaskAssignmentMailer.with(email:, task_id: task).signup_email.deliver_later
     end
+  end
 end
