1ed25a756fdf12ad4ca64d779e2aa8eb81563974
brvarner <bvarner4592@gmail.com>
2024-08-27 13:07:14 -0500

    Fixed issue with task policy that prevented task creation modal from appearing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit::Authorization
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  rescue_from Pundit::AuthorizationNotPerformedError, with: :authorization_not_performed
-
   after_action :verify_authorized, unless: :devise_controller?
   after_action :verify_policy_scoped, if: :pundit_policy_scoped?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Pundit::AuthorizationNotPerformedError, with: :authorization_not_performed
 
   private
 
@@ -22,11 +22,13 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to root_path
+    nil
   end
 
   def authorization_not_performed
     flash[:alert] = 'An authorization error occurred.'
     redirect_to root_path
+    nil
   end
 
   def pundit_policy_scoped?
