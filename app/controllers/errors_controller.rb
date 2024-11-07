c91f6512b5b3f9b972ccf8f3850eb9bdac7c33ac
brvarner <bvarner4592@gmail.com>
2024-11-07 17:42:06 -0600

    Refactored error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,10 +1,16 @@
 # app/controllers/errors_controller.rb
 class ErrorsController < ApplicationController
   def not_found
-    render status: :not_found
+    respond_to do |format|
+      format.html { render 'errors/not_found', status: :not_found }
+      format.all { render nothing: true, status: :not_found }
+    end
   end
 
   def internal_server_error
-    render status: :internal_server_error
+    respond_to do |format|
+      format.html { render 'errors/internal_server_error', status: :internal_server_error }
+      format.all { render nothing: true, status: :internal_server_error }
+    end
   end
 end
